16478559bd9de707b0563a707f2da0198c58ea6a
wanghui <wanghui@email.com>
2015-01-06 11:12:30 +0800

    1.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LovingApp::Application.config.secret_token = 'a43a799f29bd25e510e1b7769d092109ea01d5cd1ccf1baf3400f659ed5e879d831c87518e7e05351fc4d989ad5aa95a72c7b88790a4d4c959e5933ae8b736a9'
